72a277f0fce1abb8c590c112842e84b7f8745fb2
Sahil Prasad <05sahilprasad@gmail.com>
2024-02-10 00:27:05 +0530

    Chapter-Package Making

---
+++ Sumit.java
@@ -0,0 +1,12 @@
+package Package2;
+import Package1.Amit;// import java.Package1.*
+class Sumit
+{
+public static void main(String []args)
+{
+Amit a1=new Amit();
+a1.setData(5,6);
+a1.display();
+
+}
+}
