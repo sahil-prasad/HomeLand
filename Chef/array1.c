481c2eb50704b4d5234882f8ace0dae1470b00ee
Sahil Prasad <05sahilprasad@gmail.com>
2024-02-12 02:33:29 +0530

    Chef Updated

--- Chef/array1.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int marks[0]={100,99,98,97,96};
-    printf( "%d", marks[2]);
-}
--- Chef/classwork1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-    int a=5;
-    
-    printf("10");
-    getch();
-}
