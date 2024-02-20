ba543c8af3b03f078fd4532224bfa00631597691
sahil-prasad <05sahilprasad@gmail.com>
2024-02-21 01:34:34 +0530

    Chef Updated

--- Chef/HelloWorld.java
+++ Chef/HelloWorld.java
@@ -1,10 +1,6 @@
 public class HelloWorld{
-    public static void main(String[]args){
-
-        int value1=1,value2;//initialisation(declare7 give value)
-        value1=10;//assignment(giving value)
-        value2=value1*20;
-
-        System.out.println(value1+value2);
+    public static void main(String[] args){
+        System.out.print("Hello, World!");
     }
+    
 }
