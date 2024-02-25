a8fc365c5e61b822412a0bf0e51e8c0db22b51b2
sahil-prasad <05sahilprasad@gmail.com>
2024-02-26 00:49:22 +0530

    PracticeProb Updated

---
+++ PracticeProb/Program6.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class One
+{
+    static void sumCubes(int a,int b)
+    {
+        if(a%2==1)
+     {
+        for(int i=a;i<=b;i+=2)
+        {
+           int cube=i*i*i;
+           System.out.println("cube of "+i+" : "+cube);
+           int sum=+cube;
+           System.out.println("Sum of cube till "+b+ "is "+sum);
+        }
+    }
+    else
+    {
+        for(int i=a+1;i<=b;i+=2)
+        {
+           int cube=i*i*i;
+           System.out.println("cube of "+i+" : "+cube);
+           int sum=+cube;
+           System.out.println("Sum of cube till "+b+ "is "+sum);
+        }
+    }
+    }
+}
+class Program6
+{
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        One.sumCubes(a, b);
+    }
+}
